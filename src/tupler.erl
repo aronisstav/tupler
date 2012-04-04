3c7f4c2aa5e2abacd39e6aa957bef2f0314279a8
Stavros Aronis <aronisstav@gmail.com>
2012-04-04 16:32:30 +0200

    Use syntax_tools library

--- src/tupler.erl
+++ src/tupler.erl
@@ -4,6 +4,14 @@
 
 -export([analyze/1]).
 
+-record(state, {
+	  warnings       = [] :: list(),
+	  module              :: atom(),
+	  function_name       :: atom(),
+	  function_arity      :: integer(),
+	  parameter_info = [] :: list()
+	 }).
+
 -spec analyze(file:filename()) -> [term()].
 
 analyze(Filename) ->
@@ -15,16 +23,55 @@ analyze(Filename) ->
   end.
 
 analyze_abstract(Abstract) ->
-  analyze_abstract(Abstract, []).
+  analyze_abstract(Abstract, #state{}).
 
-analyze_abstract([], Warnings) ->
+analyze_abstract([], #state{warnings = Warnings}) ->
   Warnings;
-analyze_abstract([{function, Line, Name, Arity, Clauses}|Rest], Warnings) ->
-  io:format("L~w: ~w/~w\n\t~p\n",[Line, Name, Arity, Clauses]),
-  analyze_abstract(Rest, Warnings);
-analyze_abstract([Ignore|Rest], Warnings) ->
-  io:format("Ignore: ~w\n",[Ignore]),
-  analyze_abstract(Rest, Warnings).
+analyze_abstract([Form|Rest], State) ->
+  case erl_syntax:type(Form) of
+    function ->
+      Line    = erl_syntax:get_pos(Form),
+      Name    = erl_syntax:concrete(erl_syntax:function_name(Form)),
+      Arity   = erl_syntax:function_arity(Form),
+      Clauses = erl_syntax:function_clauses(Form),
+      io:format("L~w: ~w/~w\n",[Line, Name, Arity]),
+      NewState = analyze_function_clauses(Name, Arity, Clauses, State),
+      analyze_abstract(Rest, NewState);
+    attribute ->
+      case erl_syntax:concrete(erl_syntax:attribute_name(Form)) of
+	module ->
+	  [Module] = [erl_syntax:concrete(Arg) ||
+		       Arg <- erl_syntax:attribute_arguments(Form)],
+	  io:format("Module: ~w\n",[Module]),
+	  analyze_abstract(Rest, State#state{module = Module});
+	Other ->
+	  io:format("Ignore attribute ~w: ~w\n",[Other, Form]),
+	  analyze_abstract(Rest, State)
+      end;
+    Other ->
+      io:format("Ignore ~w: ~w\n",[Other, Form]),
+      analyze_abstract(Rest, State)
+  end.
 
+analyze_function_clauses(Name, Arity, Clauses, State) ->
+  ParameterInfo = lists:duplicate(Arity, []),
+  NewState =
+    State#state{function_name  = Name, function_arity = Arity,
+		parameter_info = ParameterInfo},
+  analyze_function_clauses(Clauses, NewState).
+
+analyze_function_clauses([], State) ->
+  State;
+analyze_function_clauses([Clause|Rest], State) ->
+  io:format("Clause:\n"),
+  Patterns = erl_syntax:clause_patterns(Clause),
+  Guard    = erl_syntax:clause_guard(Clause),
+  Body     = erl_syntax:clause_body(Clause),
+  io:format("Patterns:\n~p\n",[Patterns]),
+  io:format("Guard:\n~p\n",[Guard]),
+  io:format("Body:\n~p\n",[Body]),
   
+  analyze_function_clauses(Rest, State).
+  
+
 
