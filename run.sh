e0d662e28d388b9547ac7b6a2ee3c5d7686ddeed
Konstantinos Koukos <konstantinos.koukos@it.uu.se>
2012-05-27 23:01:53 +0200

    Added global statistics for test script

--- run.sh
+++ run.sh
@@ -19,19 +19,30 @@ done
 
 echo "Parsing Results."
 
+passed=0
+failed=0
+unknown=0
+
 for name in test/out/*.out.txt;
 do
 	var=$(basename $name .out.txt)
 	if [[ -e "test/correct/${var}.out.txt" ]] ; then
 		diff ${name}  test/correct/${var}.out.txt > aux.txt
 		if [[ -s "./aux.txt" ]] ; then
+			let failed+=1
 			echo "ERROR: @ $name"
 			cat aux.txt
+		else
+			let passed+=1
 		fi
 	else
+		let unknown+=1
 		echo "New FILE without base test case inserted: $name"
 	fi
 done
+
+total=$(( failed + passed + unknown ))
+echo "Quick Statistics for $total files. Passed: $passed, Failed: $failed, Unknown: $unknown "
 
 echo "Cleaning."
 rm aux.txt
